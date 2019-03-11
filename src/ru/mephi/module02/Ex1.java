7862996443b4ae507edac2a869b1c2c355736a92
Nati098 <chicken000@mail.ru>
2019-03-11 20:17:47 +0300

    added Test

--- src/ru/mephi/module02/Ex1.java
+++ src/ru/mephi/module02/Ex1.java
@@ -4,9 +4,8 @@ class Ex1 {
 
     public static void main(String[] args){
         int n = Integer.parseInt(args[0]);
-
         for(int i=1; i < 11; i++){
-            System.out.println(String.valueOf(n)+"*"+String.valueOf(i)+" = "+String.valueOf(n*i));
+            System.out.println(n+"*"+i+" = "+(n*i));
         }
     }
 }
